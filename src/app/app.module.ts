a9dc37ba52b6a37cfd3298b6b9a4f095cd0b7f77
MdOmor Faruque <mdomor.faruque@rci.rogers.com>
2024-02-22 21:26:23 -0500

    tracking updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -18,7 +20,9 @@ import { HeroBannerComponent } from './components/hero-banner/hero-banner.compon
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/hero-banner/hero-banner.component.ts
+++ src/app/components/hero-banner/hero-banner.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { OrderService } from 'src/app/services/order.service';
 
 @Component({
   selector: 'app-hero-banner',
@@ -6,5 +7,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./hero-banner.component.css']
 })
 export class HeroBannerComponent {
+
+  trackingId: string = "";
+  deliveryStatus:any = {};
+  errorStatus:string | undefined;
+
+
+  constructor(private orderService:OrderService) {
+
+  }
+
+  trackOrder() {
+    this.errorStatus = "";
+    if (this.trackingId.trim().length==0) {
+      this.errorStatus = "Please Enter a Tracking ID";
+      return;
+    }
+
+    this.orderService.getDeliveryStatus(this.trackingId).subscribe(
+      (response) => {
+        this.deliveryStatus = response;
+        this.clearTrackingId();
+      },
+      (error) => {
+        this.errorStatus = "Invalid Tracking ID";
+        this.clearTrackingId();
+      }
+    )
+
+  }
+
+  private clearTrackingId(){
+    this.trackingId = "";
+  }
 
 }
