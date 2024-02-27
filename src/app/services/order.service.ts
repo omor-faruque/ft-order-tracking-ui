6cadfbdf76624e40199032c462b9b6b1a6175678
MdOmor Faruque <mdomor.faruque@rci.rogers.com>
2024-02-27 16:10:29 -0500

    service added

--- src/app/services/order.service.ts
+++ src/app/services/order.service.ts
@@ -1,21 +1,40 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OrderService {
 
+  headers: HttpHeaders | undefined;
+  token: string | undefined;
+
   private config = {
-    hostName:"http://localhost:8080",
-    trackingUrl:"/api/orders/status?trackingId="
+    hostName: "http://localhost:8080",
+    orders: "/api/orders",
+    trackingUrl: "/api/orders/status?trackingId=",
+
   }
 
-  constructor(private http:HttpClient) { }
+  constructor(private http: HttpClient, private authService: AuthService) {
+    this.token = this.authService.getToken();
+    this.headers = new HttpHeaders({
+      'Authorization': `Bearer ${this.token}`
+    });
+  }
 
 
-  getDeliveryStatus(trackingId:string):Observable<any> {
-    return this.http.get<any>(this.config.hostName+this.config.trackingUrl+trackingId);
+  getOrders() {
+    if (this.token) {
+      return this.http.get<any>(this.config.hostName, { headers: this.headers });
+    } else {
+      return null;
+    }
+  }
+
+  getDeliveryStatus(trackingId: string): Observable<any> {
+    return this.http.get<any>(this.config.hostName + this.config.trackingUrl + trackingId);
   }
 }
